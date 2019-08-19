19831d50a0e9af63b48bc397d3557ac4bed22aeb
Ian Roskow <ianroskow@gmail.com>
2019-08-19 10:20:15 +0200

    Modal actions and minro styling.

--- portfolio/src/components/Modal.js
+++ portfolio/src/components/Modal.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div onClick={() => document.querySelector("#modal").style.display = "none"} className='background'>
+        <div onClick={() => document.querySelector("body").classList.remove("noScroll")} className='background'>
             <div onClick={(e) => e.stopPropagation()} className="modal">
-                <div className='header'>Project Title</div>
-                <div className='content'>This is the project details.</div>
-                <div className='actions'>THis is the options</div>
+                <div className='video'>Project Title</div>
+                <div className='content'>
+                    <div className='title'>Project 1</div>
+                    <div className='description'>This is the project description of how I did an awesome job with this project.</div>
+                    <div className='conntainer'>
+                        <button>Demo</button>
+                        <button>Git Hub</button>
+                    </div>
+                </div>
             </div>
         </div>,
         document.querySelector("#modal")
--- portfolio/src/components/ProjectList.js
+++ portfolio/src/components/ProjectList.js
@@ -7,6 +7,7 @@ const ProjectList = (props) => {
                 <img className='projectImage' src={props.project.image} alt={props.project.title}/>
                 <div className='projectTitle'>{props.project.title}</div>
                 <div className='projectDescription'>{props.project.description}</div>
+                <button onClick={() => document.querySelector("body").classList.add("noScroll") }>Details</button>
             </div>
         </div>
     );
