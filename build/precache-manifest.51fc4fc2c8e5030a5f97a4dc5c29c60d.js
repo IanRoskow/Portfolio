bc74883da2022aca0762a86dec2c1557b73ddc02
Ian Roskow <ianroskow@gmail.com>
2019-09-04 23:33:57 +0200

    Deploy to domain

--- build/precache-manifest.51fc4fc2c8e5030a5f97a4dc5c29c60d.js
+++ build/precache-manifest.51fc4fc2c8e5030a5f97a4dc5c29c60d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61f35d7ccd2265ad5b29936355c30293",
+    "revision": "66235739e46e739c8b334ad177a7a629",
     "url": "/index.html"
   },
   {
-    "revision": "60dc6f477eee058d1ddd",
+    "revision": "bedb004b56be42471130",
     "url": "/static/css/main.02a7700d.chunk.css"
   },
   {
@@ -12,24 +12,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2180de3d.chunk.js"
   },
   {
-    "revision": "60dc6f477eee058d1ddd",
-    "url": "/static/js/main.5d38e8ec.chunk.js"
+    "revision": "bedb004b56be42471130",
+    "url": "/static/js/main.9e50b2e1.chunk.js"
   },
   {
     "revision": "e676c0768ba9136fc741",
     "url": "/static/js/runtime~main.daaf4d4d.js"
   },
   {
-    "revision": "97bb1b327ced81c19a308aa45425dcd3",
-    "url": "/static/media/Pics.97bb1b32.png"
+    "revision": "238773a45622b3a50f5c9db8d9690800",
+    "url": "/static/media/Pics.238773a4.png"
   },
   {
     "revision": "ae33274d427dffebda6bcbf3aa7be693",
     "url": "/static/media/Pics.ae33274d.mp4"
   },
   {
-    "revision": "b8ad305e9ab78a74465c65b4dcde8613",
-    "url": "/static/media/Portfolio.b8ad305e.png"
+    "revision": "20927f29815467ace82bd4c642487dc5",
+    "url": "/static/media/Portfolio.20927f29.png"
   },
   {
     "revision": "c6f8b9dceff03ba349f284bebfff9f9b",
@@ -40,15 +40,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Todo.8ea2a5b8.mp4"
   },
   {
-    "revision": "fd9a748901605f9436dcd1cdd1af1623",
-    "url": "/static/media/Todo.fd9a7489.png"
+    "revision": "ee0a7375468e13786d1dd02004bb6e26",
+    "url": "/static/media/Todo.ee0a7375.png"
+  },
+  {
+    "revision": "5196737d779fe996121fc1512f609741",
+    "url": "/static/media/rgbGame.5196737d.png"
   },
   {
     "revision": "567fdbaae0ac30da3e8cb83fed5f4194",
     "url": "/static/media/rgbGame.567fdbaa.mp4"
-  },
-  {
-    "revision": "b5759417369d86bd7f70ccdd835a6e3a",
-    "url": "/static/media/rgbGame.b5759417.png"
   }
 ]);
