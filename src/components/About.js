0ff9874dfab9d8d2e60b042b3c55711cda133141
Ian Roskow <ianroskow@gmail.com>
2019-09-04 22:44:47 +0200

    FIxing mobile bugs

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className='about main'>
             <div className='aboutMe aboutContainer'>
                 <h1>About Me</h1>
-                <p>
+                <div className="aboutSection">
                     I am Ian Roskow, a 26 year old Front-end Developer who loves to solve problems with creative solutions.<br/><br/>
                     <div className="headers">My Past</div>
                     I graduated from Texas A&M university with Bachelors in Industrial Engineering and a Minor in Mathematics. During my time in school I took a few programming classes; and I realized I had a natural aptitude for coding. I decided to finish my degree in Engineering for the job versatility it offered but I always had the intention to become a developer. I was offered a job with a Technical Consulting firm Hitachi Consulting, and used this as my way into the development world. For the next three years I worked as a Web Developer, creating custom web solutions for Fortune 500 companies. During that time I was able to work with a range of managers, teams, and technologies to create applications that helped add value to our clients. By the time I left the company I had established myself as a Lead Front-end Developer, and a subject matter expert for Front-end coding questions for fellow employees.<br/><br/>
@@ -13,7 +13,7 @@ const About = () => {
                     In June of 2019 my family and I had the amazing opportunity to move to Barcelona, Spain, and we knew we would be remiss if we did not pursue it. So I quit my job and took a few months off to ease the transition and to focus on directing my future career path. During these last few months I have been taking online Udemy classes, building my portfolio, and doing some freelance work.<br/><br/>
                     <div className="headers">Now</div>
                     I am now looking for a full time opportunity to join a remote team of developers, where I can be challenged and develop meaningful applications. My stack, resume, and LinkedIn profile can be found below. I look forward to talking with you.
-                </p>
+                </div>
                 <div>
                     <button onClick={() => window.open(require('../assets/documents/Resume.pdf'), 'blank')}>Resume</button>
                     <button onClick={() => window.open('https://www.linkedin.com/in/ian-roskow-5b1270a6/', 'blank')}>Linked In</button>
