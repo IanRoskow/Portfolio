b57abefeeeec8294d26f30955a37c8d89d5e26dd
Ian Roskow <ianroskow@gmail.com>
2019-09-05 19:00:31 +0200

    Updates

---
+++ precache-manifest.70a8cf934e2ccf42a134fc99b0fdc474.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193815df632ab0e7f89221d752f7f4d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "000cac484912a8518bc8",
+    "url": "/static/css/main.952111be.chunk.css"
+  },
+  {
+    "revision": "c2b3fce485566d03881d",
+    "url": "/static/js/2.5aa7908d.chunk.js"
+  },
+  {
+    "revision": "000cac484912a8518bc8",
+    "url": "/static/js/main.0afeb446.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "5ccd978f1323f14e3e936b6b4054fb04",
+    "url": "/static/media/Pics.5ccd978f.png"
+  },
+  {
+    "revision": "ae33274d427dffebda6bcbf3aa7be693",
+    "url": "/static/media/Pics.ae33274d.mp4"
+  },
+  {
+    "revision": "080747bb6f66d01b51dfbc8c38e5a440",
+    "url": "/static/media/Portfolio.080747bb.png"
+  },
+  {
+    "revision": "c5c7193e0416db723608dd3d763be085",
+    "url": "/static/media/Portfolio.c5c7193e.mp4"
+  },
+  {
+    "revision": "c6f8b9dceff03ba349f284bebfff9f9b",
+    "url": "/static/media/Resume.c6f8b9dc.pdf"
+  },
+  {
+    "revision": "7cb49e9798a776cd42a8bf9d6c94a2d4",
+    "url": "/static/media/Todo.7cb49e97.png"
+  },
+  {
+    "revision": "8ea2a5b8bfed38aadcf5c41ed7a08b6d",
+    "url": "/static/media/Todo.8ea2a5b8.mp4"
+  },
+  {
+    "revision": "4a068843933d60f58dac5e9e2033ab63",
+    "url": "/static/media/rgbGame.4a068843.png"
+  },
+  {
+    "revision": "567fdbaae0ac30da3e8cb83fed5f4194",
+    "url": "/static/media/rgbGame.567fdbaa.mp4"
+  }
+]);
