db2c9fbc58e83392460061766ac94bdcdadee091
Ian Roskow <ianroskow@gmail.com>
2019-09-05 16:30:57 +0200

    Updates

--- precache-manifest.02fa1c6d49f7e691140e5e2750e9f108.js
+++ precache-manifest.02fa1c6d49f7e691140e5e2750e9f108.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66235739e46e739c8b334ad177a7a629",
+    "revision": "34ffcd1b3c89ad440f71b95b9d20c65b",
     "url": "/index.html"
   },
   {
-    "revision": "bedb004b56be42471130",
+    "revision": "862226144cd39eb6d962",
     "url": "/static/css/main.02a7700d.chunk.css"
   },
   {
@@ -12,40 +12,40 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2180de3d.chunk.js"
   },
   {
-    "revision": "bedb004b56be42471130",
-    "url": "/static/js/main.9e50b2e1.chunk.js"
+    "revision": "862226144cd39eb6d962",
+    "url": "/static/js/main.61a5ec24.chunk.js"
   },
   {
     "revision": "e676c0768ba9136fc741",
     "url": "/static/js/runtime~main.daaf4d4d.js"
   },
   {
-    "revision": "238773a45622b3a50f5c9db8d9690800",
-    "url": "/static/media/Pics.238773a4.png"
+    "revision": "5ccd978f1323f14e3e936b6b4054fb04",
+    "url": "/static/media/Pics.5ccd978f.png"
   },
   {
     "revision": "ae33274d427dffebda6bcbf3aa7be693",
     "url": "/static/media/Pics.ae33274d.mp4"
   },
   {
-    "revision": "20927f29815467ace82bd4c642487dc5",
-    "url": "/static/media/Portfolio.20927f29.png"
+    "revision": "080747bb6f66d01b51dfbc8c38e5a440",
+    "url": "/static/media/Portfolio.080747bb.png"
   },
   {
     "revision": "c6f8b9dceff03ba349f284bebfff9f9b",
     "url": "/static/media/Resume.c6f8b9dc.pdf"
   },
   {
+    "revision": "7cb49e9798a776cd42a8bf9d6c94a2d4",
+    "url": "/static/media/Todo.7cb49e97.png"
+  },
+  {
     "revision": "8ea2a5b8bfed38aadcf5c41ed7a08b6d",
     "url": "/static/media/Todo.8ea2a5b8.mp4"
   },
   {
-    "revision": "ee0a7375468e13786d1dd02004bb6e26",
-    "url": "/static/media/Todo.ee0a7375.png"
-  },
-  {
-    "revision": "5196737d779fe996121fc1512f609741",
-    "url": "/static/media/rgbGame.5196737d.png"
+    "revision": "4a068843933d60f58dac5e9e2033ab63",
+    "url": "/static/media/rgbGame.4a068843.png"
   },
   {
     "revision": "567fdbaae0ac30da3e8cb83fed5f4194",
