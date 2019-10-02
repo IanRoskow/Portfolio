ba41ded0bef999ef29150270f4282c9ab83081d6
Ian Roskow <ianroskow@gmail.com>
2019-10-02 13:23:42 +0200

    Updated skills list

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,89 @@
 import React from 'react';
 
 const About = () => {
-    return (
-        <div className='about main'>
-            <div className='aboutMe aboutContainer'>
-                <h1>About Me</h1>
-                <div className="aboutSection">
-                    I am Ian Roskow, a 26 year old Front-end Developer who loves to solve problems with creative solutions.<br/><br/>
-                    <div className="headers">My Past</div>
-                    I graduated from Texas A&M University with a Bachelor's in Industrial Engineering and a Minor in Mathematics. During my time in school, I took a few programming classes and realized my natural aptitude for coding. I decided to finish my degree in Engineering for the job versatility it offered, but I always had the intention to become a developer. I was offered a job with a technology consulting firm, Hitachi Consulting, and used this as my way into the development world. For the next three years I worked as a web developer, creating custom web solutions for Fortune 500 companies. During that time I was able to work with a range of managers, teams, and technologies to create applications that helped add value to our clients' business processes. See my resume for a detailed list of the projects I worked on. By the time I left the company I had established myself as a lead front-end developer and a front-end subject matter expert among my coworkers.<br/><br/>
-                    <div className="headers">The Opportunity</div>
-                    In June of 2019 my family and I pursued an opportunity to move to Barcelona, Spain, and we knew we would be remiss if we did not pursue it. So I quit my job and took a few months off to ease the transition and to focus on directing my future career path. During these last few months I have completed several Udemy classes, expanded my online portfolio, and obtained freelance work.<br/><br/>
-                    <div className="headers">Now</div>
-                    I am now looking for a full time opportunity to join a remote team of developers, where I can be challenged and develop meaningful applications. My stack, resume, and LinkedIn profile can be found below. I look forward to talking with you.
-                </div>
-                <div>
-                    <button onClick={() => window.open(require('../assets/documents/Resume.pdf'), 'blank')}>Resume</button>
-                    <button onClick={() => window.open('https://www.linkedin.com/in/ian-roskow-5b1270a6/', 'blank')}>Linked In</button>
-                </div>
-                <div className='myStack aboutContainer'>
-                    <h2>My Stack</h2>
-                    <div>
-                        <div className='stack'>
-                            Top Skills
-                            <small>HTML5 - CSS3 - Javascript - ReactJS - Redux - Babel - Git - Agile - Adobe XD</small>
-                        </div>
-                        <div className='stack'>
-                            Knowledgable
-                            <small>JQuery - Bootstrap - SASS - Sharepoint</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='about main'>
+      <div className='aboutMe aboutContainer'>
+        <h1>About Me</h1>
+        <div className='aboutSection'>
+          I am Ian Roskow, a 26 year old Front-end Developer who loves to solve
+          problems with creative solutions.
+          <br />
+          <br />
+          <div className='headers'>My Past</div>
+          I graduated from Texas A&M University with a Bachelor's in Industrial
+          Engineering and a Minor in Mathematics. During my time in school, I
+          took a few programming classes and realized my natural aptitude for
+          coding. I decided to finish my degree in Engineering for the job
+          versatility it offered, but I always had the intention to become a
+          developer. I was offered a job with a technology consulting firm,
+          Hitachi Consulting, and used this as my way into the development
+          world. For the next three years I worked as a web developer, creating
+          custom web solutions for Fortune 500 companies. During that time I was
+          able to work with a range of managers, teams, and technologies to
+          create applications that helped add value to our clients' business
+          processes. See my resume for a detailed list of the projects I worked
+          on. By the time I left the company I had established myself as a lead
+          front-end developer and a front-end subject matter expert among my
+          coworkers.
+          <br />
+          <br />
+          <div className='headers'>The Opportunity</div>
+          In June of 2019 my family and I pursued an opportunity to move to
+          Barcelona, Spain, and we knew we would be remiss if we did not pursue
+          it. So I quit my job and took a few months off to ease the transition
+          and to focus on directing my future career path. During these last few
+          months I have completed several Udemy classes, expanded my online
+          portfolio, and obtained freelance work.
+          <br />
+          <br />
+          <div className='headers'>Now</div>I am now looking for a full time
+          opportunity to join a remote team of developers, where I can be
+          challenged and develop meaningful applications. My stack, resume, and
+          LinkedIn profile can be found below. I look forward to talking with
+          you.
         </div>
-    )
-}
+        <div>
+          <button
+            onClick={() =>
+              window.open(require('../assets/documents/Resume.pdf'), 'blank')
+            }
+          >
+            Resume
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                'https://www.linkedin.com/in/ian-roskow-5b1270a6/',
+                'blank'
+              )
+            }
+          >
+            Linked In
+          </button>
+        </div>
+        <div className='myStack aboutContainer'>
+          <h2>My Stack</h2>
+          <div>
+            <div className='stack'>
+              Top Skills
+              <small>
+                HTML5 - CSS3 - Javascript - ReactJS - Redux - Babel - Git -
+                Agile - Adobe XD
+              </small>
+            </div>
+            <div className='stack'>
+              Knowledgable
+              <small>
+                NodeJS - Express - MongoDB - JQuery - Bootstrap - SASS -
+                Sharepoint
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,117 +3,131 @@ import ProjectList from './ProjectList';
 import Modal from './Modal';
 
 const projectMap = [
-    {
-        key: '1',
-        title: 'My Portfolio Site',
-        description: 'My portfolio site built with ReactJS and simple CSS.',
-        summary: "My portfolio site to show case my work and side projects. I built this using ReactJS and hosting it on Git Hub. I designed this site myself and decided not to use a css framework to keep the site light. I hope you enjoy it.",
-        stack: "ReactJS - CSS3 - HTML",
-        source: "https://github.com/IanRoskow/Portfolio",
-        demo: "https://ianroskow.github.io/Portfolio/",
-        video: require('../assets/images/Portfolio.mp4'),
-        image: require('../assets/images/Portfolio.png')
-    },
-    {
-        key: '2',
-        title: 'Todo',
-        description: 'An elegant jQuery todo list.',
-        summary: 'A simple jQuery todo list created using the jQuery Library, CSS3, and HTMl. This project was to experiment with simple Dom manipulation using the jQuery library. This todo list can add, mark finished, and delete items, it also is mobile responsive. This was a simple coding task in a Udemy course I took, The Web Developer Bootcamp.',
-        stack: 'jQuery - Javascript - CSS3 - Font Awesome - HTML5',
-        source: "https://github.com/IanRoskow/Todo",
-        demo: "https://ianroskow.github.io/Todo/",
-        video: require('../assets/images/Todo.mp4'),
-        image: require('../assets/images/Todo.png')
-    },
-    {
-        key: '3',
-        title: 'Pics Collection',
-        description: 'A beautiful stock image finder built with ReactJS.',
-        summary: 'A beautiful stock image search web application. This image library was built to experiment with calling APIs using axios within a React component. A search term is sent to the Unsplash API and returns 30 images that match that term. This application then arranges them into a beautiful grid that was created using CSS3. This was an assignment in a udemy course, Modern React with Redux.',
-        stack: 'ReactJS - Axios - Javascript - Semantic UI - CSS - HTML',
-        source: "https://github.com/IanRoskow/Pics",
-        demo: "https://ianroskow.github.io/Pics/",
-        video: require("../assets/images/Pics.mp4"),
-        image: require('../assets/images/Pics.png')
-    },
-    {
-        key: '4',
-        title: 'RGB Color Game',
-        description: 'A simple game to test your RGB color knowledge.',
-        summary: 'This is a simple game created with vanilla Javascript and CSS3. It has two different modes of play easy and hard, and can be reset at any point. This project was used to demonstrate ability to edit dom objects based on user interaction. This was a simple coding task in a Udemy course I took, The Web Developer Bootcamp.   ',
-        stack: 'Javascript - CSS - HTML',
-        source: "https://github.com/IanRoskow/rgbGame",
-        demo: "https://ianroskow.github.io/rgbGame/",
-        video: require('../assets/images/rgbGame.mp4'),
-        image: require('../assets/images/rgbGame.png')
-    }
-]
+  {
+    key: '1',
+    title: 'My Portfolio Site',
+    description: 'My portfolio site built with ReactJS and simple CSS.',
+    summary:
+      'My portfolio site to show case my work and side projects. I built this using ReactJS and hosting it on Git Hub. I designed this site myself and decided not to use a css framework to keep the site light. I hope you enjoy it.',
+    stack: 'ReactJS - CSS3 - HTML',
+    source: 'https://github.com/IanRoskow/Portfolio',
+    demo: 'https://ianroskow.github.io/Portfolio/',
+    video: require('../assets/images/Portfolio.mp4'),
+    image: require('../assets/images/Portfolio.png')
+  },
+  {
+    key: '2',
+    title: 'Todo',
+    description: 'An elegant jQuery todo list.',
+    summary:
+      'A simple jQuery todo list created using the jQuery Library, CSS3, and HTMl. This project was to experiment with simple Dom manipulation using the jQuery library. This todo list can add, mark finished, and delete items, it also is mobile responsive. This was a simple coding task in a Udemy course I took, The Web Developer Bootcamp.',
+    stack: 'jQuery - Javascript - CSS3 - Font Awesome - HTML5',
+    source: 'https://github.com/IanRoskow/Todo',
+    demo: 'https://ianroskow.github.io/Todo/',
+    video: require('../assets/images/Todo.mp4'),
+    image: require('../assets/images/Todo.png')
+  },
+  {
+    key: '3',
+    title: 'Pics Collection',
+    description: 'A beautiful stock image finder built with ReactJS.',
+    summary:
+      'A beautiful stock image search web application. This image library was built to experiment with calling APIs using axios within a React component. A search term is sent to the Unsplash API and returns 30 images that match that term. This application then arranges them into a beautiful grid that was created using CSS3. This was an assignment in a udemy course, Modern React with Redux.',
+    stack: 'ReactJS - Axios - Javascript - Semantic UI - CSS - HTML',
+    source: 'https://github.com/IanRoskow/Pics',
+    demo: 'https://ianroskow.github.io/Pics/',
+    video: require('../assets/images/Pics.mp4'),
+    image: require('../assets/images/Pics.png')
+  },
+  {
+    key: '4',
+    title: 'RGB Color Game',
+    description: 'A simple game to test your RGB color knowledge.',
+    summary:
+      'This is a simple game created with vanilla Javascript and CSS3. It has two different modes of play easy and hard, and can be reset at any point. This project was used to demonstrate ability to edit dom objects based on user interaction. This was a simple coding task in a Udemy course I took, The Web Developer Bootcamp.   ',
+    stack: 'Javascript - CSS - HTML',
+    source: 'https://github.com/IanRoskow/rgbGame',
+    demo: 'https://ianroskow.github.io/rgbGame/',
+    video: require('../assets/images/rgbGame.mp4'),
+    image: require('../assets/images/rgbGame.png')
+  }
+];
 
 class Projects extends React.Component {
-    state = {
-        title: "",
-        description: "",
-        summary: "",
-        stack: "",
-        source: "",
-        demo: "",
-        video: ""
-    }
+  state = {
+    title: '',
+    description: '',
+    summary: '',
+    stack: '',
+    source: '',
+    demo: '',
+    video: ''
+  };
 
+  getProjects() {
+    const projects = projectMap.map(project => {
+      return (
+        <ProjectList
+          key={project.title}
+          project={project}
+          callBack={this.updateState}
+        />
+      );
+    });
+    return projects;
+  }
 
-    getProjects() {
-        const projects = projectMap.map((project) => {
-            return <ProjectList key={project.title} project={project} callBack={this.updateState} />;
-        })
-        return projects;
-    }
+  updateState = ({
+    title,
+    description,
+    summary,
+    stack,
+    source,
+    demo,
+    video
+  }) => {
+    this.setState({
+      title,
+      description,
+      summary,
+      stack,
+      source,
+      demo,
+      video
+    });
+  };
 
-    updateState = ({title, description, summary, stack, source, demo, video}) => {
-        this.setState({
-            title,
-            description,
-            summary,
-            stack,
-            source,
-            demo,
-            video
-        });
-    }
+  clearState = () => {
+    this.setState({
+      title: '',
+      description: '',
+      summary: '',
+      stack: '',
+      source: '',
+      demo: '',
+      video: ''
+    });
+  };
 
-    clearState = () => {
-        this.setState({
-            title: "",
-            description: "",
-            summary: "",
-            stack: "",
-            source: "",
-            demo: "",
-            video: ""
-        });
-    }
-
-
-    render() {
-        return(
-            <div className="projects main">
-                <div className="myProjects">
-                    <h1>My Projects</h1>
-                    <div className='projectContainer'>
-                        {this.getProjects()}
-                    </div>
-                </div>
-                <Modal 
-                    title={this.state.title}
-                    summary={this.state.summary}
-                    stack={this.state.stack}
-                    source={this.state.source}
-                    demo={this.state.demo}
-                    video={this.state.video}
-                    callBack={this.clearState}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='projects main'>
+        <div className='myProjects'>
+          <h1>My Projects</h1>
+          <div className='projectContainer'>{this.getProjects()}</div>
+        </div>
+        <Modal
+          title={this.state.title}
+          summary={this.state.summary}
+          stack={this.state.stack}
+          source={this.state.source}
+          demo={this.state.demo}
+          video={this.state.video}
+          callBack={this.clearState}
+        />
+      </div>
+    );
+  }
 }
 
 export default Projects;
